d5c7d406e163693107df2222ece8f4be9039ae70
kaur3009 <52581294+kaur3009@users.noreply.github.com>
2019-07-28 22:21:31 -0400

    Add files via upload

---
+++ assets/Data/mySongs.ts
@@ -0,0 +1,9 @@
+import {SongInfo} from '../../app/songInfo';
+
+export const MYSONGS:SongInfo[]=[
+    {SongN:'Diamond',artistN:'Gurnam Bhullar',genre:'Popular',yearReleased:'2018',picture:'./assets/images/diamond.jpg'},
+    {SongN:'Lehnga',artistN:'Sharry Maan',genre:'Folk',yearReleased:'2018',picture:'./assets/images/lehnga.jpg'},
+    {SongN:'Kangan',artistN:'Tarsem Jassae',genre:'Jazz',yearReleased:'2019',picture:'./assets/images/kangan.jpg'},
+    {SongN:'Mera Dil',artistN:'Jass Bajwa',genre:'popular',yearReleased:'2018',picture:'./assets/images/meradil.jpg'},
+    {SongN:'Red Suit',artistN:'Karam Bajwa',genre:'Classical',yearReleased:'2017',picture:'./assets/images/redsuit.jpg'}
+];
